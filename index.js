731b30e606329c568210ac3f4928f3e7b56878e4
carlafaes <carlafaes18@hotmail.com>
2022-07-27 03:49:39 -0300

    sec

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const morgan= require('morgan');
 const routes= require('./src/routes/routes');
 const app= express();
 
+const port = process.env.PORT || 3000;
+
+
 app.use(express.json());
 app.use(morgan('dev'));
+
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 
 app.use('/api',routes);
 
